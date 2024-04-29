4296e2d08446486a62a25194053f7ee20fdb5522
Gizem Savci <129002582+GizemSavci@users.noreply.github.com>
2024-04-29 03:04:10 +0200

    Import and use BrowserRouter and Routes

--- src/client/frontend/src/App.jsx
+++ src/client/frontend/src/App.jsx
@@ -1,12 +1,24 @@
-import './App.css'
 import HomePage from './components/HomePage/HomePage';
+import  MealDetail from './components/MealDetail/MealDetail';
+import { Navbar } from './components/Navbar/Navbar';
+import { Route, Routes } from 'react-router-dom';
+import { About, Share, Meals } from './components/index';
+
+
 
 function App() {
 
   return (
     <>
       <div>
-        <HomePage />
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<HomePage />}/>
+          <Route path="/about" element={<About />}/>
+          <Route path="/share" element={<Share />}/>
+          <Route path="/meals" element={<Meals />} />
+          <Route path="/meals/:id" element={<MealDetail />} />
+        </Routes>
       </div>
     </>
   )
