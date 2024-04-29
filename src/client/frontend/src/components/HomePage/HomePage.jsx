5512ebc3de8ac9ff57f68ff3c40692daf7ad0b78
Gizem Savci <129002582+GizemSavci@users.noreply.github.com>
2024-04-29 03:00:44 +0200

    Create Homepage component

--- src/client/frontend/src/components/HomePage/HomePage.jsx
+++ src/client/frontend/src/components/HomePage/HomePage.jsx
@@ -1,9 +1,31 @@
 import MealsList from "../MealsList/MealsList"
+import Footer from "../Footer/Footer";
+import Hero from "../Hero/Hero";
+import "./HomePage.css";
+import { useNavigate } from "react-router-dom";
 
 
 function HomePage() {
+    const navigate = useNavigate();
+
     return (
-        <MealsList></MealsList>
+        <>
+            <div className="homepage-container">
+                <Hero></Hero>
+                <div className="horizontal-grid">
+                    <MealsList numMeals={2}></MealsList>
+                </div>
+                <div className="button-container">
+                    <button onClick={() => {
+                            navigate("/meals");
+                        }}
+                        >
+                            See all meals
+                    </button>
+                </div>
+                <Footer></Footer>
+            </div>
+        </>
     )
 }
 
