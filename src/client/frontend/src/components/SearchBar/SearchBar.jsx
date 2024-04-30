6413edfa65e22c3282f55372209822689f4d8036
Gizem Savci <129002582+GizemSavci@users.noreply.github.com>
2024-05-01 01:24:25 +0200

    Create search component

---
+++ src/client/frontend/src/components/SearchBar/SearchBar.jsx
@@ -0,0 +1,32 @@
+import { FaSearch } from "react-icons/fa";
+import './SearchBar.css';
+import { useState } from "react";
+
+export const SearchBar = () => {
+    const [input, setInput] = useState("");
+
+    const fetchData = (value) => {
+        fetch("https://jsonplaceholder.typicode.com/users")
+            .then((response) => response.json())
+            .then((json) => {
+                const results = json.filter((user) => {
+                    return value && user && user.name && user.name.toLowerCase().includes(value);
+                });
+                console.log(results);
+                //console.log(json);
+            })
+    }
+
+    const handleChange = (value) => {
+        setInput(value);
+        fetchData(value);
+    };
+
+    return (
+        <div className="input-wrapper">
+            <FaSearch id="search-icon" />
+            <input placeholder="Type to search..." value={input} onChange={(e) => setInput(e.target.value)}
+        />
+        </div>
+    )
+}
