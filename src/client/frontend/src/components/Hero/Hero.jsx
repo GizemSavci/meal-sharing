e73df692d50ea81186ed2952784f59129678e08c
Gizem Savci <129002582+GizemSavci@users.noreply.github.com>
2024-05-10 19:26:32 +0200

    Create Hero Text With CSS

--- src/client/frontend/src/components/Hero/Hero.jsx
+++ src/client/frontend/src/components/Hero/Hero.jsx
@@ -1,9 +1,11 @@
-import heroImage from '../../assets/images/Hero2.jpg';
+import heroImage from '../../assets/images/Hero3.jpg';
 import './Hero.css';
+import HeroTextRotating from "../HeroTextRotating/HeroTextRotating";
 
 const Hero = () => {
     return (
         <div className="hero-section">
+            <HeroTextRotating></HeroTextRotating>
             <img src={heroImage} alt="Hero image for meal sharing app"/>
         </div>
     )
---
+++ src/client/frontend/src/components/HeroTextRotating/HeroTextRotating.jsx
@@ -0,0 +1,16 @@
+import './HeroTextRotating.css';
+
+function HeroTextRotating(){
+    return (
+        <div className="wrapper">
+            <p>Share Your</p>
+            <div className="words">
+                <span>Meal</span>
+                <span>Experience</span>
+
+            </div>
+        </div>
+    )
+}
+
+export default HeroTextRotating;
